75902d98ef6355057a1ff3c40cb4a51c705b2e3d
Adrian Meca <adrianmeca@gmail.com>
2023-10-20 09:26:04 -0300

    fix user grants for mysql

--- 05.06-express-api-mikroorm-mongodb/docs/mysql-commands.sql
+++ 05.06-express-api-mikroorm-mongodb/docs/mysql-commands.sql
@@ -1,4 +1,4 @@
 create database if not exists hc4gmo;
 
 create user if not exists dsw@'%' identified by 'dsw';
-grant select, update, insert, delete on hc4gmo.* to dsw@'%';
+grant all on hc4gmo.* to dsw@'%';
